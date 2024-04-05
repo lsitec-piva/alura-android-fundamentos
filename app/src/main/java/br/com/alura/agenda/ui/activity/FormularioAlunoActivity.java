58ab1ca7064a9054527074e460e16023c782c295
lsitec-piva <lucas.piva@lsitec.org.br>
2024-04-05 08:56:24 -0300

    aula 1

---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,64 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Novo aluno";
+    @SuppressLint("StaticFieldLeak")
+    private static EditText campoNome;
+    @SuppressLint("StaticFieldLeak")
+    private static EditText campoTelefone;
+    @SuppressLint("StaticFieldLeak")
+    private static EditText campoEmail;
+
+    private final AlunoDAO dao = new AlunoDAO();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+        setTitle(TITULO_APPBAR);
+        inicializacaoDosCampos();
+        configuraBotaoSalvar();
+    }
+
+    private void configuraBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(view -> {
+            Aluno alunoCriado = criaAluno();
+            salva(alunoCriado);
+        });
+    }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salva(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+
+    @NonNull
+    private static Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+        Aluno alunoCriado = new Aluno(nome, telefone, email);
+        return alunoCriado;
+    }
+}
